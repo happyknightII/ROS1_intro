81e2b8186bc042f18f2389b689b77f46bb9e2847
Brian Lee <happyknightgaming@gmail.com>
2025-01-31 16:41:37 -0500

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/br192438/catkin_ws/src
+# Build directory: /home/br192438/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/br192438/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/br192438/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/br192438/catkin_ws/devel;/opt/ros/noetic' != '' else []
